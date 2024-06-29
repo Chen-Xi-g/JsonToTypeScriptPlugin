c499a3283099d7f9855d16237566bfdc9f8f64ac
高国峰 <ggf@ylean.com.cn>
2024-06-29 15:52:03 +0800

    fix: 适配 DevEco Studio NEXT Developer Beta1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.griffin"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("232.*")
+        untilBuild.set("233.*")
     }
 
     signPlugin {
